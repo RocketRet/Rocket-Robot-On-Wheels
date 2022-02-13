1936c97b0f3e84e7834e24954351e8f858e98bcd
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2022-02-13 12:31:38 -0500

    Fixed some incorrect INCLUDE_ASM paths

--- src/rocket/codeseg2/codeseg2_439.c
+++ src/rocket/codeseg2/codeseg2_439.c
@@ -170,10 +170,10 @@ const float D_8001DD04 = -1.0f;
 
 INCLUDE_ASM(s32, "rocket/codeseg2/codeseg2_439", func_8009AEC8);
 
-INCLUDE_ASM(s32, "rocket/codeseg2/codeseg2_441", func_8009AF20);
+INCLUDE_ASM(s32, "rocket/codeseg2/codeseg2_439", func_8009AF20);
 
-INCLUDE_ASM(s32, "rocket/codeseg2/codeseg2_441", func_8009AFB4);
+INCLUDE_ASM(s32, "rocket/codeseg2/codeseg2_439", func_8009AFB4);
 
-INCLUDE_ASM(s32, "rocket/codeseg2/codeseg2_441", func_8009B138);
+INCLUDE_ASM(s32, "rocket/codeseg2/codeseg2_439", func_8009B138);
 
-INCLUDE_ASM(s32, "rocket/codeseg2/codeseg2_441", func_8009B3B4);
+INCLUDE_ASM(s32, "rocket/codeseg2/codeseg2_439", func_8009B3B4);
